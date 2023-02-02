499d3f96c8ac7d80b505a1a7e22c12b05a21596d
LauAubert <aubertLautaroA@gmail.com>
2023-02-02 19:34:08 -0300

    dataTemplate.json and configuration added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import * as data from './dataTemplate.json';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'ArgPrograma-Front';
   isCollapsed:boolean = true;
+  data = data;
 }
