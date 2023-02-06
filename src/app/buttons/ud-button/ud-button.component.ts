dc753de770e7416cdc6834d9e9ea2b353cd4ddb7
LauAubert <aubertLautaroA@gmail.com>
2023-02-06 19:57:36 -0300

    update-delete buttons component added

---
+++ src/app/buttons/ud-button/ud-button.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-ud-button',
+  templateUrl: './ud-button.component.html',
+  styleUrls: ['./ud-button.component.css']
+})
+export class UdButtonComponent {
+  @Output() private deleteEvent = new EventEmitter<string>();
+  @Output() private updateEvent = new EventEmitter<string>();
+  @Input() set itemVar(item:any){this.item = item;}
+  item:any;
+
+  deleteEmitter(item:any) {
+    this.deleteEvent.emit(item); 
+  }
+  updateEmitter(item:any) {
+    this.updateEvent.emit(item); 
+  }
+}
