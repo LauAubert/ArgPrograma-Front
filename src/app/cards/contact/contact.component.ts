d4b03d0f82913ec1a38e0442ec999e2fccb2ba54
LauAubert <aubertLautaroA@gmail.com>
2023-02-22 20:37:13 -0300

    contact component added

---
+++ src/app/cards/contact/contact.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { BaseEditorComponent } from '../base-editor/base-editor.component';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent extends BaseEditorComponent{
+
+}
